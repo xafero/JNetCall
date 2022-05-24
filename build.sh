0d525983a2f04377859ae825f373ac9f2d27adb2
xafero <gberendt@outlook.com>
2022-05-24 03:12:07 +0200

    Porting example execution

--- build.sh
+++ build.sh
@@ -1,9 +1,24 @@
 #!/bin/sh
 
+echo ""
 cd JVM
 mvn clean package
 cd ..
 
+echo ""
 cd NET
+dotnet build
 dotnet test
 cd ..
+
+echo ""
+cd JVM
+echo "" | java -jar alienator-java/target/alienator-java.jar
+cd ..
+
+echo ""
+cd NET
+echo "" | Alienator.Sharp/bin/Debug/net6.0/Alienator.Sharp
+cd ..
+
+echo ""
