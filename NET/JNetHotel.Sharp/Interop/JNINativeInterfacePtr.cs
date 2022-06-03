fc81ac182e81044d6c428910b5da023eebb635f6
xafero <gberendt@outlook.com>
2022-06-03 03:05:20 +0200

    Disabling warning

--- NET/JNetHotel.Sharp/Interop/JNINativeInterfacePtr.cs
+++ NET/JNetHotel.Sharp/Interop/JNINativeInterfacePtr.cs
@@ -4,6 +4,8 @@ namespace JNetHotel.Sharp.Interop
 {
     internal struct JNINativeInterfacePtr
     {
+        #pragma warning disable CS0649
         public unsafe JNINativeInterface* functions;
+        #pragma warning restore CS0649
     }
 }
