c54d5e7493d6c86c0ff0a1eee308bbfc003d9a40
xafero <gberendt@outlook.com>
2022-07-15 18:45:47 +0200

    Reformatting

--- JVM/jnetcall-java-tests/src/test/java/jnetcall/java/tests/MainCallTest.java
+++ JVM/jnetcall-java-tests/src/test/java/jnetcall/java/tests/MainCallTest.java
@@ -6,11 +6,11 @@ import static jnetcall.java.client.tools.ServiceEnv.buildPath;
 
 public final class MainCallTest extends CallTest {
 
-        final String Path =
-                buildPath("..\\..\\..\\NET\\Alien1.Sharp\\bin\\Debug\\net6.0\\Alien1.Sharp.exe");
+    final String Path =
+        buildPath("..\\..\\..\\NET\\Alien1.Sharp\\bin\\Debug\\net6.0\\Alien1.Sharp.exe");
 
-        @Override
-        protected <T extends AutoCloseable> T create(Class<T> clazz) {
-                return StdIOClient.create(clazz, Path);
-        }
+    @Override
+    protected <T extends AutoCloseable> T create(Class<T> clazz) {
+        return StdIOClient.create(clazz, Path);
+    }
 }
--- JVM/jnetcall-java-tests/src/test/java/jnetcall/java/tests/NativeCallTest.java
+++ JVM/jnetcall-java-tests/src/test/java/jnetcall/java/tests/NativeCallTest.java
@@ -7,7 +7,7 @@ import static jnetcall.java.client.tools.ServiceEnv.buildPath;
 public final class NativeCallTest extends CallTest {
 
     final String Path =
-            buildPath("..\\..\\..\\NET\\Alien2.Sharp\\bin\\Debug\\net6.0\\Alien2.Sharp.dll");
+        buildPath("..\\..\\..\\NET\\Alien2.Sharp\\bin\\Debug\\net6.0\\Alien2.Sharp.dll");
 
     @Override
     protected <T extends AutoCloseable> T create(Class<T> clazz) {
