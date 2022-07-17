cf8fe0afeee41fb29dc3356e9373a87d34e91a20
xafero <gberendt@outlook.com>
2022-07-17 22:13:21 +0200

    Added array helpers

--- NET/JNetBase.Sharp/Sys/Arrays.cs
+++ NET/JNetBase.Sharp/Sys/Arrays.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -14,6 +15,31 @@ namespace JNetBase.Sharp.Sys
         public static string ToString<T>(IEnumerable<T> items)
         {
             return $"[{string.Join(", ", items)}]";
+        }
+
+        public static object[] AsObjectArray(IEnumerable items)
+        {
+            return items.Cast<object>().ToArray();
+        }
+
+        public static Array AsTypedArray(object[] args, Type type, Func<object, Type, object> convert = null)
+        {
+            convert ??= Convert.ChangeType;
+            var array = Array.CreateInstance(type, args.Length);
+            for (var i = 0; i < args.Length; i++)
+                array.SetValue(convert(args[i], type), i);
+            return array;
+        }
+
+        private static readonly Type ListType = typeof(List<>);
+
+        public static IList AsTypedArrayList(object[] args, Type type, Func<object, Type, object> convert = null)
+        {
+            convert ??= Convert.ChangeType;
+            var list = (IList)Activator.CreateInstance(ListType.MakeGenericType(type))!;
+            foreach (var arg in args)
+                list.Add(convert(arg, type));
+            return list;
         }
     }
 }
