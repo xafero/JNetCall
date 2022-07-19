7ece3b9cdf03bef4191d99ab8265b9a5641352c3
xafero <gberendt@outlook.com>
2022-07-19 22:16:42 +0200

    Adding poll interval as default

--- JVM/jnetcall-java-inproc/src/main/java/jnetcall/java/client/InProcClient.java
+++ JVM/jnetcall-java-inproc/src/main/java/jnetcall/java/client/InProcClient.java
@@ -7,7 +7,7 @@ public final class InProcClient {
 
     public static <T> T create(Class<T> clazz, String dll) {
         var pool = new ThreadExecutor();
-        var protocol = new ClrTransport(dll);
+        var protocol = new ClrTransport(dll, 15);
         var handler = new ClassProxy(protocol, pool);
         handler.listen();
         return ClientHelper.create(clazz, handler);
